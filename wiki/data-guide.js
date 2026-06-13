8bd532ead8ae5448589f98b9a47be74c87b49a09
Pokemonvoid <dcrisharrison@gmail.com>
2026-06-13 17:49:55 +1200

    Update data-guide.js

--- wiki/data-guide.js
+++ wiki/data-guide.js
@@ -25,11 +25,11 @@ window.VGUIDE = (function () {
       },
       shots: [
         "guide-img/cutscenes-events-character-selection-00.png",
-        "guide-img/cutscenes-events-character-selection-02.png",
-        "guide-img/cutscenes-events-character-selection-01.png",
         "guide-img/cutscenes-events-character-selection-03.png",
-        "guide-img/cutscenes-events-character-selection-05.png",
-        "guide-img/cutscenes-events-character-selection-04.png"
+        "guide-img/cutscenes-events-character-selection-02.png",
+        "guide-img/cutscenes-events-character-selection-03.png",
+        "guide-img/cutscenes-events-character-selection-04.png",
+        "guide-img/cutscenes-events-character-selection-05.png"
       ],
       dialogue: [
         {
